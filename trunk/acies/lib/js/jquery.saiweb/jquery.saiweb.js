9a16610162e38873b9de2aba61e644b0ad4f2979
Oneiroi <d.busby@saiweb.co.uk>
2009-02-25 12:10:57 +0000

    acies
    git-svn-id: http://svn.saiweb.co.uk/branches/acies@193 26e0a314-53de-4590-a415-dc075c606953

---
+++ trunk/acies/lib/js/jquery.saiweb/jquery.saiweb.js
@@ -0,0 +1,12 @@
+/**
+ * jquery.saiweb
+ * 
+ * This is a simple loader, will grab the latest version from svn.saiweb.co.uk
+ * ©2008 David Busby Saiweb.co.uk
+ * @see http://creativecommons.org/licenses/by-nc-sa/2.0/uk
+ * 
+ */
+ 
+ $(document).ready(function() {
+ 	$.getScript("http://svn.saiweb.co.uk/branches/jquery_plugin/tags/latest/jquery.saiweb.min.js");
+ }
